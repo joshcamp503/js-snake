9555d31c396d9a612e305361cfd3f8ee0298a8f7
joshcamp503 <joshcamp503@gmail.com>
2017-08-25 22:40:24 -0700

    Implemented gameboard structure, setup, and coordinate system

--- script.js
+++ script.js
@@ -3,20 +3,32 @@
 
 var game = {
     init: function( ) {
-        
+        game.board = {
+          spaces: [ [], [] ]
+        };
+
+        for ( i = 1; i < 41; i++ ){
+          game.board.spaces[ 0 ].push( i )
+          game.board.spaces[ 1 ].push( i )
+        };
+
         game.setup();
     },
 
     setup: function( ) {
         $( '#content' ).append( '<div id="gameboard"></div>' );
 
-        for ( i = 0; i < 1600; i++ ) {
-          $( '#gameboard' ).append(
-            $( '<div class="gamespace"> </div>' ).attr( "id", ( "d" + i ) )
-          );
-        };
+        $.each(game.board.spaces[1], function( ) {
+          var y = this;
+          $.each(game.board.spaces[0], function() {
+            var x = this;
+            $( '#gameboard' ).append(
+              $( '<div class="gamespace"> </div>' ).attr( "id", ( x + "_" + y ))
+            );
+          });
+        });
 
-        $( '#d820' ).replaceWith( '<div id="snakehead"> </div>' );
+        $( '#29_29' ).replaceWith( '<div id="snakehead"> </div>' );
     }
 }
 
