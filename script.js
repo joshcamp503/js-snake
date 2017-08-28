2c4bb8c7a074333dc789bd16988dfbaccc64adba
joshcamp503 <joshcamp503@gmail.com>
2017-08-27 20:05:23 -0700

    JS object representation implemented as well as beginning structure of functionality code

--- script.js
+++ script.js
@@ -12,15 +12,21 @@ var game = {
           game.board.spaces[ 1 ].push( i )
         };
 
+        game.snake = {
+          position: [20, 20],
+          direction: "r",
+          body: [[]]
+        };
+
         game.setup();
     },
 
     setup: function( ) {
+        // Setup gameboard and spaces
         $( '#content' ).append( '<div id="gameboard"></div>' );
-
-        $.each(game.board.spaces[1], function( ) {
+        $.each( game.board.spaces[1], function( ) {
           var y = this;
-          $.each(game.board.spaces[0], function() {
+          $.each( game.board.spaces[0], function() {
             var x = this;
             $( '#gameboard' ).append(
               $( '<div class="gamespace"> </div>' ).attr( "id", ( x + "_" + y ))
@@ -28,7 +34,24 @@ var game = {
           });
         });
 
-        $( '#29_29' ).replaceWith( '<div id="snakehead"> </div>' );
+        $( '#20_20' ).removeClass( 'gamespace' ).addClass('snake');
+
+        game.move();
+    },
+
+    getTargetID: function( ) {
+        // build functionality for "r" direction first
+        var x = game.snake.position[0];
+        var y = game.snake.position[1];
+        x++;
+        var targetID = x + "_" + y
+        return targetID;
+    },
+
+    move: function( ) {
+      var targetDiv = "#" + game.getTargetID();
+      $('.snake').removeClass( 'snake' ).addClass( 'gamespace' )
+      $(targetDiv).removeClass( 'gamespace' ).addClass('snake');
     }
 }
 
